a3037a11ade9eff4dbd6c266dd1a6f0d58d3e28a
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-05-26 23:30:55 +0100

    Create another list called author_last_names that only contains the last names of the poets in the provided string.

---
+++ 0-hello_world/splitting.py
@@ -0,0 +1,13 @@
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(",")
+names = []
+for users in author_names:
+  names = names + users.split(" ")
+string = ""
+for i in range(len(names)):
+  if i % 2 != 0:
+    string = string + names[i] + " "
+author_last_names = string.split(" ")
+author_last_names.pop()
+print(author_last_names)
